0f9f444aa05635f60cf90332b4e9c938bddd3556
Htet Khaing Win <p.htetkhaingwin@gmail.com>
2025-09-21 17:04:40 +0800

    Added Data Augmentation and training plot

---
+++ src/train_cnn_augmented.py
@@ -0,0 +1,58 @@
+import numpy as np
+import os
+import tensorflow as tf
+from tensorflow.keras import layers, models
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+
+DATA_PATH = "data/processed/"
+
+X_train = np.load(os.path.join(DATA_PATH, "X_train.npy"))
+X_test = np.load(os.path.join(DATA_PATH, "X_test.npy"))
+y_train = np.load (os.path.join(DATA_PATH, "y_train_cat.npy"))
+y_test = np.load(os.path.join(DATA_PATH, "y_test_cat.npy"))
+
+datagen = ImageDataGenerator(
+    rotation_range = 10,
+    width_shift_range = 0.1,
+    height_shift_range = 0.1,
+    zoom_range = 0.1,
+    horizontal_flip = True
+)
+datagen.fit(X_train)
+
+model = models.Sequential([
+    layers.Conv2D(32, (3,3), activation='relu', input_shape=(48,48,1)),
+    layers.MaxPooling2D((2,2)),
+    layers.Conv2D(64, (3,3), activation='relu'),
+    layers.MaxPooling2D((2,2)),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(7, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit(
+    datagen.flow(X_train, y_train, batch_size=64),
+    validation_data=(X_test, y_test),
+    epochs=10
+)
+
+os.makedirs("models", exist_ok=True)
+model.save("models/cnn_augmented.h5")
+
+# Plot learning curves
+plt.figure(figsize=(12,5))
+plt.subplot(1,2,1)
+plt.plot(history.history['accuracy'], label='train acc')
+plt.plot(history.history['val_accuracy'], label='val acc')
+plt.legend(); plt.title("Augmented Accuracy")
+
+plt.subplot(1,2,2)
+plt.plot(history.history['loss'], label='train loss')
+plt.plot(history.history['val_loss'], label='val loss')
+plt.legend(); plt.title("Augmented Loss")
+plt.show()
