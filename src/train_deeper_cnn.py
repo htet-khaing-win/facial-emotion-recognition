40a899dc5042bfc3395f125567ad7fde73b40a8c
Htet Khaing Win <p.htetkhaingwin@gmail.com>
2025-09-22 13:10:56 +0800

    Implemented deeper CNN with dropout and checkpoints

---
+++ src/train_deeper_cnn.py
@@ -0,0 +1,75 @@
+import numpy as np
+import os
+import tensorflow as tf
+from tensorflow.keras import layers, models
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+
+DATA_PATH = "data/processed/" 
+X_train = np.load(os.path.join(DATA_PATH, "X_train.npy"))
+X_test = np.load(os.path.join(DATA_PATH, "X_test.npy"))
+y_train = np.load(os.path.join(DATA_PATH, "y_train_cat.npy"))
+y_test = np.load(os.path.join(DATA_PATH, "y_test_cat.npy"))
+
+# Data augmentation
+datagen = ImageDataGenerator(
+    rotation_range = 15,
+    width_shift_range = 0.15,
+    height_shift_range = 0.15,
+    zoom_range = 0.15,
+    horizontal_flip = True
+)
+datagen.fit(X_train)
+
+# Deeper CNN Architecture
+
+model = models.Sequential([
+    layers.Conv2D(32, (3,3), activation='relu', padding='same', input_shape=(48,48,1)),
+    layers.Conv2D(32, (3,3), activation = 'relu', padding='same'),
+    layers.MaxPooling2D((2,2)),
+    layers.Dropout(0.25),
+
+    layers.Conv2D(64, (3,3), activation='relu', padding='same'),
+    layers.Conv2D(64, (3,3), activation='relu', padding='same'),
+    layers.MaxPooling2D((2,2)),
+    layers.Dropout(0.25),
+
+    layers.Conv2D(128, (3,3), activation='relu', padding='same'),
+    layers.Conv2D(128, (3,3), activation='relu', padding='same'),
+    layers.MaxPooling2D((2,2)),
+    layers.Dropout(0.25),
+
+    layers.Flatten(), #2D convert
+    layers.Dense(256, activation='relu'),
+    layers.Dropout(0.5),
+    layers.Dense(7, activation='softmax')
+
+])
+
+model.compile(optimizer='adam', #best compiler
+              loss = 'categorical_crossentropy',
+              metrics=['accuracy'])
+
+checkpoint_cb = ModelCheckpoint(
+    "models/deeper_cnn_best.h5",
+    save_best_only = True,
+    monitor = "val_accuracy",
+    mode = "max"
+) 
+
+earlystop_cb = EarlyStopping(
+    monitor = "val_accuracy",
+    patience = 5,
+    restore_best_weights = True
+)
+
+history = model.fit(
+    datagen.flow(X_train, y_train, batch_size = 64),
+    validation_data = (X_test, y_test),
+    epochs = 50,
+    callbacks = [checkpoint_cb, earlystop_cb]
+)
+
+# Save final model
+os.makedirs("models", exist_ok= True)
+model.save("models/deeper_cnn.h5")
